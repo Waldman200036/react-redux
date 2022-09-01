d106bb80ee3bf6e072e09e1090b333652f9193f6
Waldman200036 <waldman200036@gmail.com>
2022-09-01 09:10:49 -0500

    5 files modified, 2 added

--- starting-redux-template/src/App.js
+++ starting-redux-template/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
