d3698aa3b288d7098c60ec86cefeb6b57efe7e42
Walter Walden <Waldman200036@gmail.com>
2022-09-02 19:50:19 -0500

    Rendering w/o routing

--- starting-redux-template/src/App.js
+++ starting-redux-template/src/App.js
@@ -1,33 +1,22 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from 'react-router-dom';
 
-import { Navbar } from './app/Navbar';
-import { PostsList } from './features/posts/PostsList';
+import { Navbar } from "./app/Navbar";
+import { PostsList } from "./features/posts/PostsList";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <React.Fragment>
       <Navbar />
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <React.Fragment>
-                <PostsList />
-              </React.Fragment>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+      <PostsList />
+    </React.Fragment>
   );
 }
 
